ba60ec857cd8d0c88b47d8dd7daaa48b4c8f12c2
Jonathan <jdtran@ieee.org>
2019-12-31 00:59:45 -0800

    file commit

---
+++ app/src/main/java/com/example/jonathan/winter_project/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.jonathan.winter_project;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.content.Intent;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private CardView Login;
+    private TextView Reg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+       // AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+       // animationDrawable.setEnterFadeDuration(2000);
+        //animationDrawable.setExitFadeDuration(4000);
+        //animationDrawable.start();
+
+        Login = (CardView) findViewById(R.id.Login_Button);
+        Login.setOnClickListener(this);
+        Reg = (TextView) findViewById(R.id.Reg_Link);
+        Reg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.Login_Button:
+                openLoginSuccess();
+                break;
+            case R.id.Reg_Link:
+                openRegister();
+                break;
+        }
+
+    }
+
+    public void openRegister()
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+    public void openLoginSuccess()
+    {
+        Intent intent = new Intent(this, login_success.class);
+        startActivity(intent);
+    }
+}
