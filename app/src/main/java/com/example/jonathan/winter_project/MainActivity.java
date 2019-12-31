afe1550fd89b71fb8a765b2fabd1801e3852c0c7
jdtran96 <58291826+jdtran96@users.noreply.github.com>
2019-12-31 02:06:02 -0800

    made password and confirm password invisible

--- app/src/main/java/com/example/jonathan/winter_project/MainActivity.java
+++ app/src/main/java/com/example/jonathan/winter_project/MainActivity.java
@@ -1,23 +1,12 @@
 package com.example.jonathan.winter_project;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import java.util.Set;
-import java.util.ArrayList;
-import android.widget.Toast;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 import android.content.Intent;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private CardView Login;
@@ -62,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     public void openLoginSuccess()
     {
-        Intent intent = new Intent(this, login_success.class);
+        Intent intent = new Intent(this, MainPage.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/jonathan/winter_project/MainPage.java
+++ app/src/main/java/com/example/jonathan/winter_project/MainPage.java
@@ -3,7 +3,7 @@ package com.example.jonathan.winter_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login_success extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
