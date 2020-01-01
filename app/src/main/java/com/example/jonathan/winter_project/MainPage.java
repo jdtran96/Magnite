b2b7fa3862a27ccd5990e242c53935afb43534d7
jdtran96 <58291826+jdtran96@users.noreply.github.com>
2020-01-01 02:33:14 -0800

    added json imports

--- app/src/main/java/com/example/jonathan/winter_project/MainPage.java
+++ app/src/main/java/com/example/jonathan/winter_project/MainPage.java
@@ -2,6 +2,11 @@ package com.example.jonathan.winter_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainPage extends AppCompatActivity {
 
