618716ae0dbe8e6f94f6dbf4cc2bff66c902f56d
popcornjr <59491988+popcornjr@users.noreply.github.com>
2020-02-23 16:43:47 -0800

    Reupload of colorwheel selector with hex and color output.

--- app/src/main/java/com/example/jonathan/winter_project/ColorPicker.java
+++ app/src/main/java/com/example/jonathan/winter_project/ColorPicker.java
@@ -2,6 +2,8 @@ package com.example.jonathan.winter_project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.widget.EditText;
 
 import com.skydoves.colorpickerpreference.ColorEnvelope;
 import com.skydoves.colorpickerpreference.ColorListener;
@@ -10,6 +12,8 @@ import com.skydoves.colorpickerpreference.ColorPickerView;
 public class ColorPicker extends AppCompatActivity {
 
     public ColorPickerView colorPicker;
+    public EditText colorHex;
+    public CardView ColorD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,15 @@ public class ColorPicker extends AppCompatActivity {
         setContentView(R.layout.activity_color_picker);
 
         colorPicker = findViewById(R.id.colorPickerView);
+        colorHex = findViewById(R.id.Hex_Value_Edit);
+        ColorD = findViewById(R.id.colorDisplay);
 
         colorPicker.setColorListener(new ColorListener() {
             @Override
             public void onColorSelected(ColorEnvelope colorEnvelope) {
 
+               colorHex.setText(colorEnvelope.getColorHtml());
+               ColorD.setCardBackgroundColor(colorEnvelope.getColor());
             }
 
         });
